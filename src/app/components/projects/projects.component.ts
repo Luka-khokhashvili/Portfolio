01c448586f44d330508271b51a20816eb582a84e
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-05-14 16:15:43 +0400

    Restyle the projects component, clean up code, and add a gesture handler   - Added a new gesture handler to the projects component   - Installed @use-gesture/vanilla for gesture handling   - Cleaned up the code in the projects component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,13 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { Gesture } from '@use-gesture/vanilla';
+import {
+  svglAngular,
+  svglCss,
+  svglExpressjsDark,
+  svglHtml5,
+  svglJavascript,
+  svglPostgresql,
+  svglRailwayDark,
+  svglReact,
+  svglTailwindcss,
+  svglTypescript,
+  svglVercelDark,
+} from '@ng-icons/svgl';
 
 @Component({
   selector: 'app-projects',
-  imports: [CommonModule],
+  imports: [CommonModule, NgIcon],
+  providers: [
+    provideIcons({
+      svglAngular,
+      svglExpressjsDark,
+      svglReact,
+      svglTailwindcss,
+      svglTypescript,
+      svglJavascript,
+      svglHtml5,
+      svglCss,
+      svglPostgresql,
+      svglRailwayDark,
+      svglVercelDark,
+    }),
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
+  expanded = false;
+  @ViewChild('carousel', { static: true })
+  carouselRef!: ElementRef<HTMLDivElement>;
+  currentIndex = 0;
+
   projects = [
     {
       title: 'G2 Development',
@@ -15,14 +50,29 @@ export class ProjectsComponent {
         'Full-stack informational website, built for G2 development with Angular, Express, PostgreSQL',
       imgsrc: 'G2-ss.png',
       link: 'https://g2-development.vercel.app/',
-      repo: '',
+      repo: 'test',
+      stack: [
+        'svglAngular',
+        'svglExpressjsDark',
+        'svglPostgresql',
+        'svglVercelDark',
+        'svglRailwayDark',
+      ],
     },
     {
       title: 'PC Configurator',
-      description: 'Real-time PC build tool with Angular Signals',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       imgsrc: '',
       link: '',
       repo: 'https://github.com/Luka-khokhashvili/Pc-configurator-app.git',
+      stack: [
+        'svglAngular',
+        'svglExpressjsDark',
+        'svglPostgresql',
+        'svglVercelDark',
+        'svglRailwayDark',
+      ],
     },
     {
       title: 'NoteKeeper',
@@ -40,7 +90,26 @@ export class ProjectsComponent {
     },
   ];
 
-  currentIndex = 0;
+  ngAfterViewInit() {
+    const gesture = new Gesture(
+      this.carouselRef.nativeElement,
+      {
+        onDragEnd: ({ swipe: [swipeX] }) => {
+          if (swipeX === -1) this.next();
+          if (swipeX === 1) this.prev();
+        },
+      },
+      {
+        drag: {
+          axis: 'x',
+          filterTaps: true,
+          swipe: {
+            distance: 50,
+          },
+        },
+      }
+    );
+  }
 
   goTo(index: number) {
     this.currentIndex = index;
